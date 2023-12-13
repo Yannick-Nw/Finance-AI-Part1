bc5a05886a3ac084dc5da05010306dc46a2b3a4b
schamori <21.5.2003.ms@gmail.com>
2023-12-13 14:27:55 +0100

    fixed viz for short

--- commons/plot_stock_strategy.py
+++ commons/plot_stock_strategy.py
@@ -21,15 +21,16 @@ import numpy as np
 from pandas import DataFrame
 
 
-def plot_profit_fields(ax: Axes, signals: list[bool], chart: list[float], dates, ):
+def plot_profit_fields(ax: Axes, signals: list[bool], chart: list[float], dates, algo_course=True):
     max_y = max(chart)
     last_action_entry = (0, chart[0])
     for i, signal in enumerate(signals[1:], start=1):
-        if signal != signals[i - 1]:
+        if signal != signals[i - 1] or i == len(signals) - 1:
             last_index, last_entry = last_action_entry
             if signals[i - 1]:
-                _plot_entry(ax,  dates, signals[i - 1], last_index, i, last_entry, chart[i], max_y)
+                _plot_entry(ax,  dates, signals[i - 1], last_index, i, last_entry, chart[i], max_y, algo_course)
             last_action_entry = (i, chart[i])
+
 
 
 def _plot_entry(
@@ -40,7 +41,8 @@ def _plot_entry(
     second_index,
     first_entry,
     second_entry,
-    max_y
+    max_y,
+    algo_course
 ):
     """
     index (int): The index of the stock entry.
@@ -48,7 +50,11 @@ def _plot_entry(
     total_screen_height (float): The difference between the first and the last closing prices in the dataset.
     total_screen_width (float): The total width of the screen/plot.
     """
-    color = "red" if (second_entry - first_entry) < 0 else "green"
+    if algo_course:
+        color = "red" if (second_entry - first_entry) < 0 else "green"
+    else:
+        color = "red" if (second_entry - first_entry) * signal < 0 else "green"
+
     borders = [0, max(first_entry, second_entry)] if signal > 0 else [min(first_entry, second_entry), max_y]
 
     # Add borders to the fill
